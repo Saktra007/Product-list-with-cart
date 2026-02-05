4822cfbfb085a4c9e88ed616d45b74841e57aea8
Saktra007 <gamingst410@gmail.com.com>
2026-02-05 21:12:25 +0700

    add data product

---
+++ assets/js/data.js
@@ -0,0 +1,130 @@
+export const products = [
+  {
+    id: 1,
+    name: "Waffle with Berries",
+    category: "Waffle",
+    price: 6.5,
+    image:
+      "https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=500",
+  },
+  {
+    id: 2,
+    name: "Vanilla Bean Crème Brûlée",
+    category: "Crème Brûlée",
+    price: 7.0,
+    image:
+      "https://images.pexels.com/photos/2092906/pexels-photo-2092906.jpeg?auto=compress&cs=tinysrgb&w=500",
+  },
+  {
+    id: 3,
+    name: "Macaron Mix of Five",
+    category: "Macaron",
+    price: 8.0,
+    image:
+      "https://images.pexels.com/photos/239581/pexels-photo-239581.jpeg?auto=compress&cs=tinysrgb&w=500",
+  },
+  {
+    id: 4,
+    name: "Classic Tiramisu",
+    category: "Tiramisu",
+    price: 5.5,
+    image:
+      "https://images.pexels.com/photos/1209029/pexels-photo-1209029.jpeg?auto=compress&cs=tinysrgb&w=500",
+  },
+  {
+    id: 5,
+    name: "Pistachio Baklava",
+    category: "Baklava",
+    price: 4.0,
+    image:
+      " https://tse1.mm.bing.net/th/id/OIP.9bq0M3e9nYeVx7kdMbqYHQHaFA?pid=Api&P=0&h=220", // ឬប្រើ Link ខាងក្រោម
+  },
+  {
+    id: 6,
+    name: "Lemon Meringue Pie",
+    category: "Pie",
+    price: 5.0,
+    image:
+      "https://images.pexels.com/photos/2693447/pexels-photo-2693447.jpeg?auto=compress&cs=tinysrgb&w=500",
+  },
+  {
+    id: 7,
+    name: "Red Velvet Cake",
+    category: "Cake",
+    price: 4.5,
+    image:
+      "https://images.pexels.com/photos/2144112/pexels-photo-2144112.jpeg?auto=compress&cs=tinysrgb&w=500",
+  },
+  {
+    id: 8,
+    name: "Fresh Strawberry Tart",
+    category: "Tart",
+    price: 5.25,
+    image:
+      "https://images.pexels.com/photos/461431/pexels-photo-461431.jpeg?auto=compress&cs=tinysrgb&w=500",
+  },
+  {
+    id: 9,
+    name: "Chocolate Fudge Brownie",
+    category: "Brownie",
+    price: 3.5,
+    image:
+      "https://images.pexels.com/photos/45202/brownie-dessert-cake-sweet-45202.jpeg?auto=compress&cs=tinysrgb&w=500",
+  },
+  {
+    id: 10,
+    name: "Matcha Green Tea",
+    category: "Drink",
+    price: 4.75,
+    image:
+      "https://images.pexels.com/photos/1581484/pexels-photo-1581484.jpeg?auto=compress&cs=tinysrgb&w=500",
+  },
+  {
+    id: 11,
+    name: "Blueberry Cheesecake",
+    category: "Cake",
+    price: 6.0,
+    image:
+      "https://images.pexels.com/photos/1126359/pexels-photo-1126359.jpeg?auto=compress&cs=tinysrgb&w=500",
+  },
+  {
+    id: 12,
+    name: "Caramel Popcorn Sundae",
+    category: "Ice Cream",
+    price: 5.5,
+    image:
+      "https://images.pexels.com/photos/1352245/pexels-photo-1352245.jpeg?auto=compress&cs=tinysrgb&w=500",
+  },
+  {
+    id: 13,
+    name: "Assorted Mochi",
+    category: "Mochi",
+    price: 4.25,
+    image:
+      "https://images.pexels.com/photos/6662822/pexels-photo-6662822.jpeg?auto=compress&cs=tinysrgb&w=500",
+  },
+  {
+    id: 14,
+    name: "Apple Cinnamon Galette",
+    category: "Pie",
+    price: 5.75,
+    image:
+      "https://images.pexels.com/photos/616833/pexels-photo-616833.jpeg?auto=compress&cs=tinysrgb&w=500",
+  },
+  {
+    id: 15,
+    name: "Iced Caramel Macchiato",
+    category: "Drink",
+    price: 4.5,
+    image:
+      "https://images.pexels.com/photos/2396220/pexels-photo-2396220.jpeg?auto=compress&cs=tinysrgb&w=500",
+  },
+  {
+    id: 16,
+    name: "Glazed Donuts",
+    category: "Donut",
+    price: 3.99,
+    image:
+      "https://images.pexels.com/photos/1191639/pexels-photo-1191639.jpeg?auto=compress&cs=tinysrgb&w=500",
+  },
+];
